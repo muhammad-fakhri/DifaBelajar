6cfc01743af8bee0f48c3da21a4c66633a6a15c4
muhammad-fakhri <muhammad_fakhri@apps.ipb.ac.id>
2018-11-16 17:48:57 +0700

    HAHAHHA

--- test.php
+++ test.php
@@ -1,12 +1,18 @@
-<?php 
-	echo "Hallo Semua, Aku Fakhri";
-	echo "HAI SEMUA";
-	$kata = 4;
-	if ($kata) {
-		echo "Kalian Jelek !!!!";
-	}
-	echo "Halo";
-	echo "string";
-	echo "hahahaah";
-	echo "Usman Jelek";
- ?>
+<html>
+	<title></title>
+	<body>
+	<?php 
+		echo "Hallo Semua, Aku Fakhri";
+		echo "HAI SEMUA";
+		$kata = 4;
+		if ($kata) {
+			echo "Kalian Jelek !!!!";
+		}
+		echo "Halo";
+		echo "string";
+		echo "hahahaah";
+		echo "Usman Jelek";
+	 ?>
+		<h1>HOHOHOHOHOHO</h1>
+	</body>
+</html>
