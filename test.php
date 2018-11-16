b50f37bfde39127a495230708ba74d72f7950fbd
muhammad-fakhri <muhammad_fakhri@apps.ipb.ac.id>
2018-11-16 17:45:33 +0700

    Commitan

--- test.php
+++ test.php
@@ -1,12 +1,19 @@
-<?php 
-	echo "Hallo Semua, Aku Fakhri";
-	echo "HAI SEMUA";
-	$kata = 4;
-	if ($kata) {
-		echo "Kalian Jelek !!!!";
-	}
-	echo "Halo";
-	echo "string";
-	echo "hahahaah";
-	echo "mater banget";
- ?>
+ <html>
+ 	<title></title>
+ 	<body>
+	<?php 
+		echo "Hallo Semua, Aku Fakhri";
+		echo "HAI SEMUA";
+		$kata = 4;
+		if ($kata) {
+			echo "Kalian Jelek !!!!";
+		}
+		echo "Halo";
+		echo "string";
+		echo "hahahaah";
+		echo "mater banget";
+	 ?>
+ 		
+ 		<h1>KALIMAT CUY</h1>
+ 	</body>
+ </html>
